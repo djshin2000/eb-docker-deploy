b0cd8176b311baeb67228db2e81f4e04852e0fbc
Shin Dongjin <djshin2000@gmail.com>
2018-03-15 12:34:54 +0900

    dev, production.py 에 주석 추가

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -19,4 +19,5 @@ set_config(secrets, module_name=__name__, start=True)
 # Media(User-uploaded files)를 위한 스토리지
 DEFAULT_FILE_STORAGE = 'config.storage.DefaultFilesStorage'
 # Static files(collectstatic)을 위한 스토리지
+# 아래 주석처리 이유: S3대신 EC2에서 정적파일 제공 (프리티어 Put 사용량 절감)
 # STATICFILES_STORAGE = 'config.storage.StaticFilesStorage'
